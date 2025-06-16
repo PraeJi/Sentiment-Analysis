d6d89a99ab152c6523e7288f82e5342bdc9cbfc3
PraeJirapa <ramjan_j@silpakorn.edu>
2025-06-16 16:30:33 +0700

    edit env

--- start_ngrok.py
+++ start_ngrok.py
@@ -1,6 +1,10 @@
+import os
 from pyngrok import ngrok, conf
 from sentiment_text import app
-conf.get_default().auth_token = "2y7mXCOBPrbMNYDxweVvzk6Dnou_3vc3rAyhNd2BGTHhz4dTR"
+
+conf.get_default().auth_token = os.getenv("NGROK_AUTHTOKEN")
+#conf.get_default().auth_token = "2y7mXCOBPrbMNYDxweVvzk6Dnou_3vc3rAyhNd2BGTHhz4dTR"
+
 public_url = ngrok.connect(8000)
 print(" * ngrok tunnel:", public_url)
 
